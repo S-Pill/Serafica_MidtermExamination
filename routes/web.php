6cb66b67ef4963fd306d7bc1402ebc2b58b46afd
M303user <m303user@M303.uc-bcf.edu.ph>
2024-06-20 18:48:04 +0800

    Modify route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 use App\Http\Controllers\ProductController;
+
+
 Route::get('/products', [ProductController::class, 'index']);
-    
